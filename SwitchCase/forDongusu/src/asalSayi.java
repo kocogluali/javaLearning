0b067cf2b9e4c6185b37b1a85028723877f17d77
Ali Kemal KOÇOĞLU <kocogluali@yandex.com.tr>
2022-09-18 23:34:51 +0300

    For Döngüsü Kullanımı Kullanımı

---
+++ SwitchCase/forDongusu/src/asalSayi.java
@@ -0,0 +1,20 @@
+package SwitchCase.forDongusu.src;
+
+public class asalSayi {
+    public static void main(String[] args) {
+        int asalSayi=11;
+        int sonuc;
+        for (int sayi=2 ; sayi<asalSayi; sayi++) {
+           sonuc=asalSayi%sayi;
+           if (sonuc==0) {
+            System.err.println("Sonuç asal sayı değildir.");
+            break;
+           }
+           if(sayi+1==asalSayi){
+            System.err.println("Sonuç asal sayıdır.");
+           }
+         
+        }
+        }
+    }
+
