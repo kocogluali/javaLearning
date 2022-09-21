90b572e5aac904971148af10990abc29f6d37c0b
Ali Kemal KOÇOĞLU <kocogluali@yandex.com.tr>
2022-09-21 21:51:18 +0300

    javaOpp

---
+++ oop1/src/Main.java
@@ -0,0 +1,13 @@
+package oop1.src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("oop1 merhaba");
+        Product produck1 = new Product();
+        produck1.name="Kahve Makinesi";
+        produck1.unitsInStock=10;
+        produck1.discount=7;
+        produck1.unitPrice=1786.8;
+        produck1.imageURL="https://www.hepsiburada.com/ali.jpg";
+    }
+}
---
+++ oop1/src/Product.java
@@ -0,0 +1,9 @@
+package oop1.src;
+
+public class Product {
+    String name;
+    double discount;
+    double unitPrice;
+    int unitsInStock;
+    String imageURL;
+}
