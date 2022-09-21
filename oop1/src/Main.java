a6b9e3a9ff2d3760680edc784f381badec319235
Ali Kemal KOÇOĞLU <kocogluali@yandex.com.tr>
2022-09-21 21:52:10 +0300

    merhaba

--- oop1/src/Main.java
+++ oop1/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("oop1 merhaba");
         Product produck1 = new Product();
         produck1.name="Kahve Makinesi";
-        produck1.unitsInStock=10;
+        produck1.unitsInStock=30;
         produck1.discount=7;
         produck1.unitPrice=1786.8;
         produck1.imageURL="https://www.hepsiburada.com/ali.jpg";
